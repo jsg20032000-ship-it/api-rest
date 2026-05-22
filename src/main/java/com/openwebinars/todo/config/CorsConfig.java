0fcdb6d0cc4852c28ba26ed3245b321881595227
Jorge Suárez Gallegos <yxn56090@educastur.es>
2026-05-22 15:33:20 +0200

    Añadir CORS para GitHub Pages

--- src/main/java/com/openwebinars/todo/config/CorsConfig.java
+++ src/main/java/com/openwebinars/todo/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:63342")
+                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:63342", "https://jsg20032000-ship-it.github.io")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
